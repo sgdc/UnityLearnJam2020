c0c3655d0a149fc248f7d75710c4207a6b2015cc
Wes LaFerriere <wesley.laferriere@gmail.com>
2020-09-19 13:00:54 -0400

    Initial commit

---
+++ Assets/Scripts/EnemyControllerBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControllerBehavior : MonoBehaviour
+{
+    PlayerControllerBehavior player;
+
+    public float wanderConstant = 7.5f;
+    public float speed = 0.1f;
+    public Vector3 slide;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerControllerBehavior>();
+    }
+
+    public bool CanBeStruck()
+    {
+        Vector3 meToPlayerVector = player.transform.position - this.transform.position;
+        float distanceToPlayer = meToPlayerVector.magnitude;
+        float angleFromFront = Vector3.Angle(this.transform.forward, meToPlayerVector);
+        return 
+            distanceToPlayer < PlayerControllerBehavior.strikeDistance
+            && angleFromFront > PlayerControllerBehavior.minStrikeAngleFromFront;
+    }
+
+    void UpdateMaterial() {
+        if (CanBeStruck())
+            this.GetComponent<Renderer>().material.color= new Color(1.0f, 0.5f, 0.5f);
+        else
+            this.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 0.0f);
+    }
+
+    void Update()
+    {
+        float rotation = (
+            Mathf.Sin(Time.time) * 0.3f
+            + Mathf.Sin(Time.time * 0.5f) * 0.2f
+            + Mathf.Sin(Time.time * 30.0f) * 0.3f
+            + Mathf.Sin(Time.time * 23.0f) * 0.2f
+        ) * Random.Range(0, wanderConstant);
+        this.transform.Rotate(Vector3.up, rotation);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(player.transform.position - this.transform.position), 0.02f);
+
+        slide *= 0.9f;
+        this.transform.position += slide + this.transform.forward * speed;
+
+        UpdateMaterial();
+    }
+}
---
+++ Assets/Scripts/PlayerControllerBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerBehavior : MonoBehaviour
+{
+    public static float strikeDistance = 6.0f;
+    public static float minStrikeAngleFromFront = 50.0f;
+
+    const float acceleration = 0.01f;
+    const float mouseSensitivity = 2.5f;
+    Vector3 velocity;
+
+    bool dead = false;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if(dead) {
+            if (Input.GetKey(KeyCode.Space))
+             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            return;
+        }
+
+        Vector3 instantAcceleration = (this.transform.right * Input.GetAxis("Horizontal") + this.transform.forward * Input.GetAxis("Vertical"));
+        instantAcceleration = acceleration * instantAcceleration.normalized;
+        velocity += instantAcceleration;
+        velocity *= 0.9f;
+
+        this.transform.position += velocity;
+
+        float mouseRotation = Input.GetAxis("Mouse X");
+        this.transform.Rotate(Vector3.up, mouseRotation);
+
+        if(Input.GetMouseButton(0)) {
+            EnemyControllerBehavior enemy = FindObjectOfType<EnemyControllerBehavior>();
+            if(enemy.CanBeStruck()) {
+                Vector3 difference = enemy.transform.position - this.transform.position;
+                this.transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(difference, Vector3.up), Vector3.up);
+                this.transform.position += difference / 2.0f;
+                enemy.transform.position += difference / 1.5f;
+                enemy.speed *= 1.5f;
+                enemy.wanderConstant *= 0.8f;
+                enemy.transform.localScale *= 0.75f;
+                enemy.slide = difference.normalized * 2.0f;
+                if(enemy.transform.localScale.x < 0.4f) {
+                    Destroy(enemy);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        dead = true;
+        this.GetComponent<Renderer>().enabled = false;
+    }
+}
